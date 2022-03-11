c13c4b4b18144bc4e2ced8eea5ad6952b14aebdc
Bryn Bennett <bryn.t.bennett@gmail.com>
2022-03-11 13:19:05 +1300

    FIX: usage pagination

--- src/app/viewing-area/pages/view-usage/view-usage.component.ts
+++ src/app/viewing-area/pages/view-usage/view-usage.component.ts
@@ -1,34 +1,28 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
 
-import { UsageInfo } from '../../../core/objects/usageInfo';
-import { BoatUsageService } from './providers/boat-usage.service';
-
+import { UsageInfo } from "../../../core/objects/usageInfo";
+import { BoatUsageService } from "./providers/boat-usage.service";
 
 @Component({
-  selector: 'app-view-usage',
-  templateUrl: './view-usage.component.html',
-  styleUrls: ['./view-usage.component.css']
+  selector: "app-view-usage",
+  templateUrl: "./view-usage.component.html",
+  styleUrls: ["./view-usage.component.css"],
 })
 export class ViewUsageComponent implements OnInit {
-
   // MatPaginator Output
   pageEvent: PageEvent;
   totalNumberItems = 0;
-  pageSizeOptions = [5, 10, 20, 40, 50, 100];
+  pageSizeOptions = [5, 10, 20, 40, 50, 100, 10000];
   usages;
 
-  constructor(
-    public usageService: BoatUsageService,
-  ) { }
+  constructor(public usageService: BoatUsageService) {}
 
   ngOnInit() {
-    this.usageService.totalNumberOfUsages.subscribe(val => {
-      this.totalNumberItems = val;
+    this.usageService.currentSelectedUsages.subscribe((val) => {
+      this.usages = val;
     });
-    this.usageService.currentSelectedUsages.subscribe(val => { this.usages = val })
   }
-
 
   getUsages(event?: PageEvent) {
     let offset = -1;
@@ -36,13 +30,12 @@ export class ViewUsageComponent implements OnInit {
       return this.usageService.updateBatch(event.pageSize);
     }
     if (event.pageIndex > event.previousPageIndex) {
-      return this.usageService.forwardBatch(event.pageSize - 1)
+      return this.usageService.forwardBatch(event.pageSize - 1);
       offset = event.pageSize - 1;
     } else if (event.pageIndex < event.previousPageIndex) {
-      return this.usageService.backBatch(event.pageSize - 1)
+      return this.usageService.backBatch(event.pageSize - 1);
     }
   }
-
 
   trackByIdx(i) {
     return i;
