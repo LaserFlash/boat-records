ed061995ddd8cd66cd205b51753999c2f359384b
Bryn Bennett <brynbennett@catalyst.net.nz>
2023-02-21 23:33:44 +0000

    TECH: increment version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  version: '0.2.0',
+  version: "0.2.1",
   firebase: {
     apiKey: "AIzaSyCuC9g6sVhTYoPSqOg8yVmZXX1f7JEXPZ4",
     authDomain: "qcyc-usagemaintenancetracker.firebaseapp.com",
     databaseURL: "https://qcyc-usagemaintenancetracker.firebaseio.com",
     projectId: "qcyc-usagemaintenancetracker",
     storageBucket: "qcyc-usagemaintenancetracker.appspot.com",
-    messagingSenderId: "317211543661"
-  }
+    messagingSenderId: "317211543661",
+  },
 };
