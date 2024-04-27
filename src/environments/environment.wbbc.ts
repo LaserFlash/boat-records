9b3c9d2f5635ca91835733f831eb798474bf24a3
Bryn Bennett <bryn.t.bennett@gmail.com>
2024-04-27 23:08:23 +0000

    FEAT(WBBC): inital wbbc site

---
+++ src/environments/environment.wbbc.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEmK8yFVxc7sESHwKAk7xIOzA-vjJKvJY",
+    authDomain: "wbbc-boats.web.app",
+    databaseURL: "https://wbbc-boats-default-rtdb.firebaseio.com/",
+    projectId: "wbbc-boats",
+    storageBucket: "wbbc-boats.appspot.com",
+    messagingSenderId: "332405672542",
+  },
+};
+
+export const title = 'WBBC'
