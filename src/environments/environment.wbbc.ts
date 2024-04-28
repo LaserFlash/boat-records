d897a8c24a5680a06e121e9aaf792f0bf5f36147
Bryn Bennett <bryn.t.bennett@gmail.com>
2024-04-28 00:44:15 +0000

    TECH(environments): cleanup configs

--- src/environments/environment.wbbc.ts
+++ src/environments/environment.wbbc.ts
@@ -3,10 +3,9 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyCEmK8yFVxc7sESHwKAk7xIOzA-vjJKvJY",
     authDomain: "wbbc-boats.web.app",
-    databaseURL: "https://wbbc-boats-default-rtdb.firebaseio.com/",
+    databaseURL: "https://wbbc-boats-default-rtdb.firebaseio.com",
     projectId: "wbbc-boats",
     storageBucket: "wbbc-boats.appspot.com",
-    messagingSenderId: "332405672542",
   },
 };
 
